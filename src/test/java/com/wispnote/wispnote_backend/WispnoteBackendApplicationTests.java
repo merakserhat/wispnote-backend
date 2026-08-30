293c3636b4d53405fe538a7035ff572665f44ab9
Serhat Merak <merakserhat@gmail.com>
2026-08-30 18:31:42 +0300

    initial commit

---
+++ src/test/java/com/wispnote/wispnote_backend/WispnoteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wispnote.wispnote_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WispnoteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
